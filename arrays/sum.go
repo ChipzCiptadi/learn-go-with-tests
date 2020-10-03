dfcc4bfba58707b5668cb46abc7fa599b85d5676
ChipzCiptadi <chipz.ciptadi@gmail.com>
2020-10-03 08:09:15 +0000

    sum all, sum all tails

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,3 +9,28 @@ func Sum(numbers []int) int {
 
 	return result
 }
+
+func SumAll(numbers ...[]int) []int {
+	var sum []int
+
+	for _, number := range numbers {
+		sum = append(sum, Sum(number))
+	}
+
+	return sum
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	var sum []int
+
+	for _, number := range numbers {
+		if len(number) == 0 {
+			sum = append(sum, 0)
+		} else {
+			tail := number[1:]
+			sum = append(sum, Sum(tail))
+		}
+	}
+
+	return sum
+}
